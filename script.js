3e476c3ffd3ef8f540397657f238b897d01e6d11
Lucy Lloyd <lucyeadam@gmail.com>
2022-01-31 14:53:32 +0000

    adapted so different grids can be made

--- script.js
+++ script.js
@@ -3,26 +3,50 @@ let container = document.getElementById("container");
 tester.className = "segment";
 container.appendChild(tester);*/
 
-//deciding size of grid
-let boxNo = 50;
 
+//put in info line saying to fill in grid number
+let instructions = document.createElement('h3');
+instructions.innerHTML = "Enter grid column number (max 100)";
+instructions.style.fontFamily = "Helvetica";
+container.appendChild(instructions);
+
+//setting up button with input
+let input = document.createElement('input');
+input.setAttribute("type", "number");
+input.id= "input";
+container.appendChild(input);
+let button = document.createElement('button');
+button.innerHTML= "submit";
+container.appendChild(button);
+
+
+//setting up an initial box size
+
+
+function setup(boxNo){
+  
+ 
 //creating a new div for a line of segments
 for (let j=0; j<=boxNo-1; j++) {
 let newLine = document.createElement("div");
 newLine.className = "newLine";
-newLine.id = "line[j]";
+
 container.appendChild(newLine);
 
 //filling each line with the different segments
 for (let i=0; i<= boxNo-1; i++) {
   const newUnit = document.createElement("div");
   newUnit.className = "adding";
-newUnit.id = "unit[i]";
+
   newLine.appendChild(newUnit);
   
 };
 
 };
+}
+
+
+
 
 
 
@@ -36,11 +60,19 @@ testing.style.color = colours[Math.floor(Math.random() * colours.length)];
 
 })*/
 
+
+
+
+button.onclick = function()
+{setup(document.getElementById('input').value)
+colouring()};
+
+
+function colouring (){
 let newColor = document.querySelectorAll(".adding");
 for (const color of newColor ) {
   color.addEventListener("mouseover", () => {
     color.style.backgroundColor = colours[Math.floor(Math.random() * colours.length)];
   })
 }
-
-
+}
